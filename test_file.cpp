d958657cb39e16320970393e529f75300022bac1
rev <rev@example.com>
2026-08-07 12:15:50 -0700

    Added test file for c++ source file and gitignore

--- test_file.cpp
+++ test_file.cpp
@@ -1,1 +1,14 @@
 #include <vector>
+
+// Macro short for macro instruction that then determine what is triggered for the smaller ones
+#if defined(_MSC_VER)
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__((visibility("default")))
+#endif
+
+extern "C" {
+    EXPORT int AddNumbers(int a, int b) {
+        return a + b;
+    }
+}
